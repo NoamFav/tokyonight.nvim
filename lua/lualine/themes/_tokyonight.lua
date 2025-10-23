c5b0b652c55e4d45fff7de9a26cf0084d15a6ba7
NoamFav <95364866+NoamFav@users.noreply.github.com>
2025-10-23 20:01:52 +0200

    feat(lua/lualine/themes/_tokyonight): Make center part of lua-line themes transparent in various modes

--- lua/lualine/themes/_tokyonight.lua
+++ lua/lualine/themes/_tokyonight.lua
@@ -11,38 +11,43 @@ function M.get(style)
   hl.normal = {
     a = { bg = colors.blue, fg = colors.black },
     b = { bg = colors.fg_gutter, fg = colors.blue },
-    c = { bg = colors.bg_statusline, fg = colors.fg_sidebar },
+    c = { bg = "none", fg = colors.fg_sidebar }, -- Make the center part transparent
   }
 
+  hl.inactive = {
+    a = { bg = "none", fg = colors.blue }, -- Transparent inactive section
+    b = { bg = "none", fg = colors.fg_gutter, gui = "bold" },
+    c = { bg = "none", fg = colors.fg_gutter },
+  }
+  -- Modify for other modes if needed
   hl.insert = {
     a = { bg = colors.green, fg = colors.black },
     b = { bg = colors.fg_gutter, fg = colors.green },
+    c = { bg = "none", fg = colors.fg_sidebar }, -- Transparent in insert mode
   }
 
   hl.command = {
     a = { bg = colors.yellow, fg = colors.black },
     b = { bg = colors.fg_gutter, fg = colors.yellow },
+    c = { bg = "none", fg = colors.fg_sidebar },
   }
 
   hl.visual = {
     a = { bg = colors.magenta, fg = colors.black },
     b = { bg = colors.fg_gutter, fg = colors.magenta },
+    c = { bg = "none", fg = colors.fg_sidebar },
   }
 
   hl.replace = {
     a = { bg = colors.red, fg = colors.black },
     b = { bg = colors.fg_gutter, fg = colors.red },
+    c = { bg = "none", fg = colors.fg_sidebar },
   }
 
   hl.terminal = {
     a = { bg = colors.green1, fg = colors.black },
     b = { bg = colors.fg_gutter, fg = colors.green1 },
-  }
-
-  hl.inactive = {
-    a = { bg = colors.bg_statusline, fg = colors.blue },
-    b = { bg = colors.bg_statusline, fg = colors.fg_gutter, gui = "bold" },
-    c = { bg = colors.bg_statusline, fg = colors.fg_gutter },
+    c = { bg = "none", fg = colors.fg_sidebar },
   }
 
   if config.lualine_bold then
